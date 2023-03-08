d82157f9f4134e9377ad5f2ba8a52d874a808a6f
Álvaro Pérez <alvaro.perezla@alum.uca.es>
2023-03-08 15:07:23 +0100

    Comprobar remove

--- temas/encapsulacion/Scala/Lista.scala
+++ temas/encapsulacion/Scala/Lista.scala
@@ -10,7 +10,7 @@ trait ListaInterfaz[T] {
     def last(): T
     def isEmpty(): Boolean
     def length(): Int
-    // def clone()
+    def copy(): Any
 
     //Al crear una clase que hereda de la interfaz no puedo poner como parámetro otro tipo,
     //Pues estaríamos comparando tipos distintos
@@ -41,7 +41,7 @@ trait Iterator [E] {
     override def last(): T = {l.last}
     override def isEmpty(): Boolean = {l.isEmpty}
     override def length(): Int = {l.size}
-    // def clone(): MiLista[T] 
+    override def copy(): MiLista[T] = this;//new MiLista[E]();
     //override def isEqualTo(other: MiLista[T]): Boolean = {other.l == l}
 
     override def equals(arg0: Any): Boolean = {
@@ -50,37 +50,42 @@ trait Iterator [E] {
       arg0 == l;
     }
 
-    override def iterator(): Iterator[T] = new MyIterator[T](l)
+    //Si ponemos el tipo de devolucion sea Iterator[T] entonces no podríamos utilizar más
+    //Métodos que los que vienen declarados en el trait.
+    override def iterator(): MyIterator[T] = new MyIterator[T](l)
 
     //Método de conversión a cadena
     override def toString() = {l.toString()}
   }
 
-    class MyIterator[E](elements: Vector[E]) extends Iterator[E] {
-        private var currentIndex = 0
+class MyIterator[E](var elements: Vector[E]) extends Iterator[E] {
+      private var currentIndex = 0
 
-        override def hasNext(): Boolean = currentIndex < elements.size
+      override def hasNext(): Boolean = {currentIndex < elements.size}
 
-        override def next(): E = {
-            var element = elements(currentIndex)
-            // if(hasNext){
-            //     val element = elements(currentIndex)
-            //     currentIndex += 1
-            //     element
-            // }else{
-            //     val element = new E();
-            // }
-            element
-        }
-        
-        override def remove(): Unit = {
-            if (currentIndex > 0) {
-            
+      override def next(): E = {
+          var element = elements(currentIndex)
+          if(hasNext){
+              currentIndex += 1
+              element = elements(currentIndex)
+              //element
+          }
+          element
+      }
+
+      //Método añadido para facilitar las pruebas
+      def get(): E = {elements(currentIndex);}
+      
+      override def remove(): Unit = {
+          
+          elements = elements.zipWithIndex.filter(x => x._2!=currentIndex).map(x=>x._1).toVector
             //elements = elements.patch(currentIndex, Nil, 1)
+          if (currentIndex > 0) {
             currentIndex -= 1
-            }
-        }
-    }
+          }else if(currentIndex == 0 && hasNext)
+            currentIndex +=1
+      }
+}
 
 object Lista {
         def main(args: Array[String]) ={
@@ -90,12 +95,33 @@ object Lista {
             l1.addFirst(1);
             println(l1);
 
-            println("\n Creamos una segunda lista de Int...")
-            var l2: MiLista[Int] = new MiLista[Int]();
-            println("Añadimos el numero 1, comparamos y mostramos el resultado...")
-            l2.addFirst(1);
+            println("\nCopiamos la anterior a una segunda lista de Int...")
+            var l2: MiLista[Int] = l1.copy();
+            println("Comparamos las dos y mostramos el resultado...")
             var comparition = l1.equals(l2);
             println(comparition);
+
+            println("Seguimos metiendo en la primera lista por el final...")
+            l1.addLast(2);
+            l1.addLast(3);
+            println(l1);
+
+            println("\nPasamos a comprobar el uso de iteradores...")
+            var it = l1.iterator();
+            println("Mostramos que es el primer elemento y comprobamos si tiene siguiente...")
+            println(it.get());
+            println(it.hasNext())
+            println("Vamos al siguiente y lo mostramos")
+            it.next()
+            println(it.get());
+            println("Eliminamos el elemento actual y mostramos en cuál nos quedamos")
+            it.remove()
+            println(it.get())
+            println("Vamos al siguiente y lo mostramos...")
+            it.next()
+            println(it.get())
+            println("Pero mostramos el estado actual de la lista...")
+            println(l1)
         }
 }
 
