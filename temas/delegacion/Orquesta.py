2225e5964b8923b2f6885305cd77de0b63c414b5
elperez264 <alvaro.perezla@alum.uca.es>
2023-03-12 20:49:27 +0100

    sin terminar

---
+++ temas/delegacion/Orquesta.py
@@ -0,0 +1,20 @@
+from Instrumentos import *
+
+
+class Orquesta():
+    def __init__(self):
+        self.instrumentos = []
+
+    def addInstrumento(self, ins):
+        self.instrumentos.append(ins)
+
+    def removeInstrumento(self, ins):
+        self.instrumentos.remove(ins)
+
+    def tocar(self):
+        for i in self.instrumentos:
+            i.tocar()
+    
+    def afinar(ins):
+        ins.afinar()
+        ins.tocar()
