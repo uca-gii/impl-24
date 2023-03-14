568ff18b1ea400559087b07cb709a4d44bec38bf
elperez264 <alvaro.perezla@alum.uca.es>
2023-03-14 17:22:03 +0100

    Documentación interna

--- temas/delegacion/Python/Orquesta.py
+++ temas/delegacion/Python/Orquesta.py
@@ -5,7 +5,7 @@ class IteradorOrquesta():
     #El iterador necesita de los instrumentos (lista) sobre el que iterar
     def __init__(self, instrumentos):
         self.ins = instrumentos
-        self.currentIndex = 0
+        self.indiceActual = 0
         self.size = len(self.ins)
 
     def __iter__(self):
@@ -13,9 +13,9 @@ class IteradorOrquesta():
     
     def __next__(self):
         
-        if self.currentIndex < self.size:
-            instrumento = self.ins[self.currentIndex]
-            self.currentIndex +=1
+        if self.indiceActual < self.size:
+            instrumento = self.ins[self.indiceActual]
+            self.indiceActual +=1
             return instrumento
     
         raise StopIteration
@@ -23,6 +23,7 @@ class IteradorOrquesta():
 
 class Orquesta():
     def __init__(self):
+        #Lista dinámica
         self.__instrumentos = []
 
     def addInstrumento(self, ins):
@@ -39,5 +40,6 @@ class Orquesta():
         ins.afinar()
         ins.tocar()
 
+    #Delegamos el trabajo de iteracion
     def __iter__(self):
         return IteradorOrquesta(self.__instrumentos)
