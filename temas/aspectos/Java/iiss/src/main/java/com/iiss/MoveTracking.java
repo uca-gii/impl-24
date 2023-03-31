f05025000192c702914ca6d1e3237c2ee2d40b85
elperez264 <alvaro.perezla@alum.uca.es>
2023-03-31 20:30:45 +0200

    segunda version

--- temas/aspectos/Java/iiss/src/main/java/com/iiss/MoveTracking.java
+++ temas/aspectos/Java/iiss/src/main/java/com/iiss/MoveTracking.java
@@ -2,6 +2,7 @@ package com.iiss;
 
 import java.util.*;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.JoinPoint;
 
 @Aspect
 public class MoveTracking {
@@ -12,10 +13,21 @@ public class MoveTracking {
     return result;
   }
 
-  @After("execution(* com.iiss.Line.*(..)) && args(fe)")
-  public void after(FigureElement fe) {
+  // @After("execution(* com.iiss.Line.*(..)) && args(fe)")
+  // public void after(FigureElement fe) {
+  //   System.out.println("After");
+  //   movees.add(fe);}
+
+  @After("execution(* com.iiss.Line.*(..)) || execution(* com.iiss.Point.*(..))")
+  public void after(JoinPoint jp) {
     System.out.println("After");
-    movees.add(fe);}
+    Object[] args = jp.getArgs();
+    for (Object arg : args) {
+      if (arg instanceof FigureElement) {
+        movees.add((FigureElement) arg);
+      }
+    }
+  }
 
   // public void after(){
   //   System.out.println("After");
