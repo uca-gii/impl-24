a5fdae975f23bbf26679a33c4e3b570c6ca466f4
Marta Jones <marta.jonesgonzalez@alum.uca.es>
2024-04-29 18:56:38 +0200

    Update calculator.rb

--- temas/errores/ruby/calculator.rb
+++ temas/errores/ruby/calculator.rb
@@ -1,28 +1,27 @@
 
 # Calculator.rb
 
-require 'optional'
 
 class Calculator
 
     def divide(dividend, divisor)
-        return Optional.empty unless dividend.is_a?(Numeric) && divisor.is_a?(Numeric)
-        return Optional.empty if divisor == 0
-        Optional.of( dividend / divisor )
+        return nil unless dividend.is_a?(Numeric) && divisor.is_a?(Numeric)
+        return nil if divisor == 0
+        dividend / divisor
     end
 
     def sum(a, b)
-        return Optional.empty unless a.is_a?(Numeric) && b.is_a?(Numeric)
-        Optional.of(a + b)
+        return nil unless a.is_a?(Numeric) && b.is_a?(Numeric)
+        a + b
     end
 
     def subtract(a, b)
-        return Optional.empty unless a.is_a?(Numeric) && b.is_a?(Numeric)
-        Optional.of(a - b)
+        return nil unless a.is_a?(Numeric) && b.is_a?(Numeric)
+        a - b
     end
 
     def multiply(a, b)
-        return Optional.empty unless a.is_a?(Numeric) && b.is_a?(Numeric)
-        Optional.of(a * b)
+        return nil unless a.is_a?(Numeric) && b.is_a?(Numeric)
+        a * b
     end
-end
+en
