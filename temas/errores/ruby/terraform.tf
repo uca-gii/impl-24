73a2da55372833cf27ea15b8e38ad8b4e80b9a9c
Marta Jones <martajon10@gmail.com>
2024-04-29 22:25:36 +0200

    Update terraform.tf

--- temas/errores/ruby/terraform.tf
+++ temas/errores/ruby/terraform.tf
@@ -1,9 +1,10 @@
+# terraform
 
 provider "null" {}
 
 resource "null_resource" "start_server" {
   provisioner "local-exec" {
-    command     = "python3 -m http.server 8084 &"
-    working_dir = path.module
+    command     = "python3 -m http.server 8083 &"
+    working_dir = "${path.module}"
   }
 }
