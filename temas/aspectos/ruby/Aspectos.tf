789838c92eeee47f75454c8599124145667571bd
SBaston <101277911+SBaston@users.noreply.github.com>
2024-05-06 18:52:18 +0200

    Modificado Terraform

--- temas/aspectos/ruby/Aspectos.tf
+++ temas/aspectos/ruby/Aspectos.tf
@@ -1,36 +1,38 @@
 
-provider "null" {}
-
-resource "null_resource" "install_ruby" {
-  provisioner "local-exec" {
-    command = "choco install ruby -y"
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
   }
 }
 
-resource "null_resource" "install_sinatra" {
-  depends_on = [null_resource.install_ruby]
-
-  provisioner "local-exec" {
-    command = "gem install sinatra"
-    
-  }
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
 }
 
-resource "null_resource" "install_aquarium" {
-  depends_on = [null_resource.install_ruby]
-
-  provisioner "local-exec" {
-    command = "gem install aquarium"
-    
-  }
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+  keep_locally = false
 }
 
-resource "null_resource" "run_app" {
-  depends_on = [null_resource.install_sinatra]
+resource "docker_container" "ubuntu" {
+  image = docker_image.ubuntu.image_name
+  name = "ubuntu_latest"
+  ports {
+    internal = 80
+    external = 4568
+  }
+  provisioner "local_exec" {
+    command = sudo apt update
+    command = sudo apt install ruby -y
+    command = gem install sinatra
+    command = gem install aquarium
+    command = sudo apt intall wget -y
+    command = wget -P . https://github.com/martaajonees/impl-24/blob/feat-aspectos/temas/aspectos/ruby/appAspectos.rb
+    command = ruby appAspectos.rb
+  }
+}
   
 
-  provisioner "local-exec" {
-    command     = "ruby appAspectos.rb"
-    working_dir = path.module
-  }
-}
