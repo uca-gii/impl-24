11e8f2fc7b38aa64340b7f07715e920b544f5f63
Alberto-mp <albertops4conil@gmail.com>
2024-05-06 15:13:38 +0200

    Proyecto sbt creado y se pasan los tests. Falta automatización con Actions y archivo README.md

---
+++ temas/abstraccion/scala/proyecto/build.sbt
@@ -0,0 +1,7 @@
+name := "proyecto"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
---
+++ temas/abstraccion/scala/proyecto/src/test/scala/tests.scala
@@ -0,0 +1,40 @@
+package ejemplo
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+// Importa las clases que quieres probar
+import ejemplo.{Animal, Mamifero, Volador, Caminante, Nadador}
+
+class AnimalSpec extends AnyFlatSpec with Matchers {
+
+  "An Animal" should "fly" in {
+    val animal = new Animal with Volador
+    animal.volar() shouldBe "¡Estoy volando!"
+  }
+
+  it should "swim" in {
+    val animal = new Animal with Nadador
+    animal.nadar() shouldBe "¡Estoy nadando!"
+  }
+
+  it should "walk" in {
+    val animal = new Animal with Caminante
+    animal.caminar() shouldBe "¡Estoy caminando!"
+  }
+
+  it should "have today's date" in {
+    val animal = new Animal
+    animal.fecha() shouldBe "¡Hoy es jueves!"
+  }
+
+  "A Mamifero" should "swim" in {
+    val ballena = new Mamifero with Nadador
+    ballena.nadar() shouldBe "¡Estoy nadando!"
+  }
+
+  it should "reproduce" in {
+    val ballena = new Mamifero
+    ballena.reproduccion() shouldBe "Soy mamifero!"
+  }
+}
