1b6b5df94cdea39db431a40b315ef9d86b691160
SBaston <101277911+SBaston@users.noreply.github.com>
2024-05-10 12:37:44 +0200

    Subidos archivos para la web

---
+++ temas/lambdas/ruby/Lambdas.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+resource "docker_image" "app_image" {
+  name = "my-sinatra-app:latest"
+  build {
+    context = path.module
+    dockerfile = "Dockerfile"
+  }
+  
+}
+
+resource "docker_container" "app_container" {
+  image = docker_image.app_image.name
+  name = "mi-app-container"
+  ports {
+    internal = 4568
+    external = 4568
+  }
+}
