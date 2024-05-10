d30fcd0384a6032e4b6359ebd73daa11756d18bf
SBaston <101277911+SBaston@users.noreply.github.com>
2024-05-10 15:34:11 +0200

    Actualizado el test unitario Lambdas RUBY

--- temas/lambdas/ruby/testLambdas.rb
+++ temas/lambdas/ruby/testLambdas.rb
@@ -1,57 +1,36 @@
 require 'minitest/autorun'
-require 'rx'
+require_relative 'lambdas.rb' 
 
-class TestTemperatureSensor < Minitest::Test
-  # Método para simular las lecturas de temperatura de un sensor
-  def simulate_temperature_sensor(sensor_id)
-    Rx::Observable.create do |observer|
-      # Simulamos la generación de lecturas de temperatura cada segundo
-      Thread.new do
-        loop do
-          temperature_celsius = rand(10..45)
-          observer.on_next({ sensor_id: sensor_id, temperature_celsius: temperature_celsius }) # Emite un hash con el ID del sensor y la temperatura en Celsius
-          sleep(1)
-        end
-      end
+class TestTemperatureSensorSimulation < Minitest::Test
+  def test_simulate_temperature_sensor
+    sensor_id = 1
+    sensor_data = []
 
-      Thread.new do
-        sleep(15)
-        observer.on_completed
-      end
+    # Observador para capturar datos del sensor
+    observer = Rx::Observer.create(
+      lambda { |data| sensor_data << data },
+      lambda { |error| puts "Error en el sensor: #{error}" },
+      lambda { puts "La secuencia del sensor ha finalizado\n" }
+    )
+
+    # Simulamos el sensor
+    simulate_temperature_sensor(sensor_id).subscribe(observer)
+
+    # Esperamos un tiempo para que se capturen algunas lecturas
+    sleep(5)
+
+    # Comprobamos si se han capturado algunos datos del sensor
+    assert_operator sensor_data.size, :>, 0
+  end
+
+  def test_run_temperature_monitoring_simulation
+    # Capturamos la salida estándar para verificar los mensajes
+    output = capture_io do
+      run_temperature_monitoring_simulation
     end
-  end
 
-  # Método para probar la conversión de Celsius a Fahrenheit
-  def test_celsius_to_fahrenheit_conversion
-    data = { temperature_celsius: 20 } # Temperatura en Celsius para probar
-    converted_data = to_fahrenheit.call(data)
-    expected_fahrenheit = 68.0 # Valor esperado en Fahrenheit para 20°C
-    assert_equal(expected_fahrenheit, converted_data[:temperature_fahrenheit])
-  end
-
-  # Método para probar la detección de lecturas anómalas
-  def test_anomalous_reading_detection
-    normal_data = { temperature_celsius: 30 } # Temperatura normal para probar
-    anomalous_data = { temperature_celsius: 40 } # Temperatura anómala para probar
-    assert_equal(false, is_anomalous.call(normal_data)) # La lectura normal no debe ser anómala
-    assert_equal(true, is_anomalous.call(anomalous_data)) # La lectura anómala debe ser detectada como anómala
-  end
-
-  private
-
-  # Definimos la constante para la conversión de Celsius a Fahrenheit
-  CELSIUS_TO_FAHRENHEIT_CONSTANT = 32.0
-
-  # Lambda para convertir la temperatura a grados Fahrenheit
-  def to_fahrenheit
-    ->(data) do
-      temperature_fahrenheit = data[:temperature_celsius] * 9.0 / 5.0 + CELSIUS_TO_FAHRENHEIT_CONSTANT
-      data.merge(temperature_fahrenheit: temperature_fahrenheit)
-    end
-  end
-
-  # Lambda para verificar si una lectura es anómala
-  def is_anomalous
-    ->(data) { data[:temperature_celsius] > 35 }
+    # Comprobamos si se imprimieron los mensajes de finalización de la simulación para ambos sensores
+    assert_match(/Sensor 1 - La secuencia ha finalizado\n/, output[0])
+    assert_match(/Sensor 2 - La secuencia ha finalizado\n/, output[0])
   end
 end
