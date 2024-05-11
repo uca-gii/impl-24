decf60773c40773298f03f67af5583e90bbee754
Jose Agustin <joseagustin.cabreracastillo@gmail.com>
2024-05-11 15:41:11 +0200

    Creacion de Proyecto de Anotaciones  y de tests

---
+++ temas/anotaciones/Tests_Anotaciones/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tests_Anotaciones;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
