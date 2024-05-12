f2a5a449298e83a8c3cf48bc13bf8f5652911a42
SBaston <101277911+SBaston@users.noreply.github.com>
2024-05-12 19:20:24 +0200

    Versión index 2.0 y app

--- temas/abstraccion/ruby/appAbstraccion.rb
+++ temas/abstraccion/ruby/appAbstraccion.rb
@@ -5,6 +5,45 @@ get '/' do
     erb :index
 end
 
+get '/Circulo/:radio' do
+    radio = params[:radio]
+    circulo = Figura::Circulo.new(radio)
+    output = StringIO.new
+    $stdout = output
+    puts "Círculo: "
+    puts "Área: #{circulo.area}"
+    puts "Perímetro: #{circulo.perimetro}"
+    $stdout = STDOUT
+    output.string
+end
 
+get '/Rectangulo/:ancho/:alto' do
+    ancho = params[:ancho]
+    alto = params[:alto]
+    rectangulo = Figura::Rectangulo.new(ancho, alto)
+    output = StringIO.new
+    $stdout = output
+    puts "Rectángulo: "
+    puts "Área: #{rectangulo.area}"
+    puts "Perímetro: #{rectangulo.perimetro}"
+    $stdout = STDOUT
+    output.string
+end
+
+get '/Triangulo/:base/:altura/:lado1/:lado2/:lado3' do
+    base = params[:base]
+    altura = params[:altura]
+    lado1 = params[:lado1]
+    lado2 = params[:lado2]
+    lado3 = params[:lado3]
+    triangulo = Figura::Triangulo.new(base, altura, lado1, lado2, lado3)
+    output = StringIO.new
+    $stdout = output
+    puts "Triángulo: "
+    puts "Área: #{triangulo.area}"
+    puts "Perímetro: #{triangulo.perimetro}"
+    $stdout = STDOUT
+    output.string
+end
 
 set :port, 4568
