a142d688a6e0684467a2b5cb87a4576825040d65
FedeLo13 <fede.lopezpe@alum.uca.es>
2024-05-20 16:03:45 +0200

    Modificación al test de la clase persona

--- temas/herencia/dart/test/persona_test.dart
+++ temas/herencia/dart/test/persona_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:test/test.dart';
 import 'package:dart/persona.dart';
 
@@ -6,12 +8,28 @@ void main() {
     // Crear una persona para los tests
     var persona = Persona('Test', 30);
 
-    test('hablar devuelve el mensaje correcto', () {
-      // Ejecutar hablar y capturar el resultado
-      var resultado = persona.hablar();
+    test('hablar devuelve el mensaje correcto', () async {
+      // Capturar la salida estándar de print
+      var output = await capturePrint(() {
+        persona.hablar();
+      });
 
       // Verificar que el resultado sea el esperado
-      expect(resultado, 'Hola, mi nombre es Test y tengo 30 años');
+      expect(output, 'Hola, mi nombre es Test y tengo 30 años.\n');
     });
   });
+}
+
+// Función para capturar la salida estándar de print
+Future<String> capturePrint(Function function) async {
+  var output = '';
+  var zone = Zone.current.fork(
+    specification: ZoneSpecification(print: (self, parent, zone, line) {
+      output += '$line\n';
+    }),
+  );
+  await zone.run(() async {
+    function();
+  });
+  return output;
 }
